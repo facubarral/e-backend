535dc0d6895fd5143e7444ccea0e35e6e1c591a3
facubarral <156373434+facubarral@users.noreply.github.com>
2026-02-20 13:09:11 +0000

    Initial Spring Boot project

---
+++ distribucontrol/src/main/java/com/facundo/distribucontrol/DistribucontrolApplication.java
@@ -0,0 +1,13 @@
+package com.facundo.distribucontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistribucontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistribucontrolApplication.class, args);
+	}
+
+}
---
+++ distribucontrol/src/test/java/com/facundo/distribucontrol/DistribucontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facundo.distribucontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistribucontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
